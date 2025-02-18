b8f9133c25fb9fc72bd04aa06d95ddbd6c2e7630
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2025-02-18 16:47:04 +0100

    Cleaned up rotation command

--- Commands/Modifying/Rotation/SubCommands/Add.cs
+++ Commands/Modifying/Rotation/SubCommands/Add.cs
@@ -1,91 +1,4 @@
-﻿/*
-// -----------------------------------------------------------------------
-// <copyright file="Add.cs" company="MapEditorReborn">
-// Copyright (c) MapEditorReborn. All rights reserved.
-// Licensed under the CC BY-SA 3.0 license.
-// </copyright>
-// -----------------------------------------------------------------------
-
-namespace MapEditorReborn.Commands.ModifyingCommands.Rotation.SubCommands
-{
-    using System;
-    using API.Extensions;
-    using API.Features.Objects;
-    using CommandSystem;
-    using Events.EventArgs;
-    using Events.Handlers.Internal;
-    using Exiled.API.Features;
-    using Exiled.Permissions.Extensions;
-    using UnityEngine;
-    using static API.API;
-
-    /// <summary>
-    /// Modifies object's rotation by adding a certain value to the current one.
-    /// </summary>
-    public class Add : ICommand
-    {
-        /// <inheritdoc/>
-        public string Command => "add";
-
-        /// <inheritdoc/>
-        public string[] Aliases { get; } = Array.Empty<string>();
-
-        /// <inheritdoc/>
-        public string Description => string.Empty;
-
-        /// <inheritdoc/>
-        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-        {
-            if (!sender.CheckPermission("mpr.rotation"))
-            {
-                response = "You don't have permission to execute this command. Required permission: mpr.rotation";
-                return false;
-            }
-
-            Player player = Player.Get(sender);
-            if (!player.TryGetSessionVariable(SelectedObjectSessionVarName, out MapEditorObject mapObject) || mapObject == null)
-            {
-                if (!ToolGunHandler.TryGetMapObject(player, out mapObject))
-                {
-                    response = "You haven't selected any object!";
-                    return false;
-                }
-
-                ToolGunHandler.SelectObject(player, mapObject);
-            }
-
-            if (!mapObject.IsRotatable)
-            {
-                response = "You can't modify this object's rotation!";
-                return false;
-            }
-
-            if (arguments.Count >= 3 && TryGetVector(arguments.At(0), arguments.At(1), arguments.At(2), out Vector3 newRotation))
-            {
-                ChangingObjectRotationEventArgs ev = new(player, mapObject, newRotation);
-                Events.Handlers.MapEditorObject.OnChangingObjectRotation(ev);
-
-                if (!ev.IsAllowed)
-                {
-                    response = ev.Response;
-                    return true;
-                }
-
-                mapObject.EulerAngles += ev.Rotation;
-                player.ShowGameObjectHint(mapObject);
-
-                response = ev.Rotation.ToString("F3");
-                return true;
-            }
-
-            response = "Invalid values.";
-            return false;
-        }
-    }
-}
-*/
-
-using CommandSystem;
+﻿using CommandSystem;
 using LabApi.Features.Wrappers;
 using ProjectMER.Features;
 using ProjectMER.Features.Extensions;
@@ -95,9 +8,9 @@ using static ProjectMER.Features.Extensions.VectorExtensions;
 
 namespace ProjectMER.Commands.Modifying.Rotation.SubCommands;
 
-    /// <summary>
-    /// Modifies object's rotation by adding a certain value to the current one.
-    /// </summary>
+/// <summary>
+/// Modifies object's rotation by adding a certain value to the current one.
+/// </summary>
 public class Add : ICommand
 {
 	/// <inheritdoc/>
