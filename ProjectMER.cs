954a8d556806665f5dacbac6873d571bdbca2c10
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2025-05-08 16:05:15 +0200

    Bump

--- ProjectMER.cs
+++ ProjectMER.cs
@@ -76,7 +76,7 @@ public class ProjectMER : Plugin<Config>
 
 	public override string Author => "Michal78900";
 
-	public override Version Version => new Version(2025, 5, 2, 1);
+	public override Version Version => new Version(2025, 5, 8, 1);
 
 	public override Version RequiredApiVersion => new Version(1, 0, 0, 0);
 }
