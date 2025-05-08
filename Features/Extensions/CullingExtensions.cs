6c79c6d80e07e20ac05ff389c8650390b60ef8e3
Michal78900 <75911188+Michal78900@users.noreply.github.com>
2025-05-08 13:41:37 +0200

    Small culling extension fixes

--- Features/Extensions/CullingExtensions.cs
+++ Features/Extensions/CullingExtensions.cs
@@ -10,47 +10,48 @@ using Objects;
 /// </summary>
 public static class CullingExtensions
 {
-    /// <summary>
-    /// Spawns the given <paramref name="schematic"/> for the specified <paramref name="player"/>.
-    /// </summary>
-    /// <param name="player">The target.</param>
-    /// <param name="schematic">The schematic to spawn.</param>
-    public static void SpawnSchematic(this Player player, SchematicObject schematic)
-    {
-        foreach (NetworkIdentity networkIdentity in schematic.NetworkIdentities)
-            player.SpawnNetworkIdentity(networkIdentity);
-    }
+	private static MethodInfo? _sendSpawnMessage;
 
-    /// <summary>
-    /// Destroys the given <paramref name="schematic"/> for the specified <paramref name="player"/>.
-    /// </summary>
-    /// <param name="player">The target.</param>
-    /// <param name="schematic">The schematic to destroy.</param>
-    public static void DestroySchematic(this Player player, SchematicObject schematic)
-    {
-        foreach (NetworkIdentity networkIdentity in schematic.NetworkIdentities)
-            player.DestroyNetworkIdentity(networkIdentity);
-    }
+	/// <summary>
+	/// Gets a NetworkServer.SendSpawnMessage's <see cref="MethodInfo"/>.
+	/// </summary>
+	private static MethodInfo SendSpawnMessage => _sendSpawnMessage ??= typeof(NetworkServer).GetMethod("SendSpawnMessage", BindingFlags.NonPublic | BindingFlags.Static);
 
-    /// <summary>
-    /// Spawns the given <paramref name="networkIdentity"/> for the specified <paramref name="player"/>.
-    /// </summary>
-    /// <param name="player">The target.</param>
-    /// <param name="networkIdentity">The network identity to spawn.</param>
-    public static void SpawnNetworkIdentity(this Player player, NetworkIdentity networkIdentity)
-    {
-        MethodInfo? sendSpawnMessage = typeof(NetworkServer).GetMethod("SendSpawnMessage", BindingFlags.NonPublic | BindingFlags.Static);
-        if (sendSpawnMessage == null)
-            return;
+	/// <summary>
+	/// Spawns the given <paramref name="schematic"/> for the specified <paramref name="player"/>.
+	/// </summary>
+	/// <param name="player">The target.</param>
+	/// <param name="schematic">The schematic to spawn.</param>
+	public static void SpawnSchematic(this Player player, SchematicObject schematic)
+	{
+		foreach (NetworkIdentity networkIdentity in schematic.NetworkIdentities)
+			player.SpawnNetworkIdentity(networkIdentity);
+	}
 
-        sendSpawnMessage.Invoke(null, new object[] { networkIdentity, player.Connection });
-    }
+	/// <summary>
+	/// Destroys the given <paramref name="schematic"/> for the specified <paramref name="player"/>.
+	/// </summary>
+	/// <param name="player">The target.</param>
+	/// <param name="schematic">The schematic to destroy.</param>
+	public static void DestroySchematic(this Player player, SchematicObject schematic)
+	{
+		foreach (NetworkIdentity networkIdentity in schematic.NetworkIdentities)
+			player.DestroyNetworkIdentity(networkIdentity);
+	}
 
-    /// <summary>
-    /// Destroys the given <paramref name="networkIdentity"/> for the specified <paramref name="player"/>.
-    /// </summary>
-    /// <param name="player">The target.</param>
-    /// <param name="networkIdentity">The network identity to destroy.</param>
-    public static void DestroyNetworkIdentity(this Player player, NetworkIdentity networkIdentity) =>
-        player.Connection.Send(new ObjectDestroyMessage { netId = networkIdentity.netId });
+	/// <summary>
+	/// Spawns the given <paramref name="networkIdentity"/> for the specified <paramref name="player"/>.
+	/// </summary>
+	/// <param name="player">The target.</param>
+	/// <param name="networkIdentity">The network identity to spawn.</param>
+	public static void SpawnNetworkIdentity(this Player player, NetworkIdentity networkIdentity) =>
+		SendSpawnMessage.Invoke(null, [networkIdentity, player.Connection]);
+
+	/// <summary>
+	/// Destroys the given <paramref name="networkIdentity"/> for the specified <paramref name="player"/>.
+	/// </summary>
+	/// <param name="player">The target.</param>
+	/// <param name="networkIdentity">The network identity to destroy.</param>
+	public static void DestroyNetworkIdentity(this Player player, NetworkIdentity networkIdentity) =>
+		player.Connection.Send(new ObjectDestroyMessage { netId = networkIdentity.netId });
 }
